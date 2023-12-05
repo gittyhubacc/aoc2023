38945fc60f16081c27abb5e34c5c81d84d879c44
based <based@arch>
2023-12-05 02:36:11 -0500

    day 5 part 2

--- day5/day5.lua
+++ day5/day5.lua
@@ -1,109 +1,136 @@
 #!/usr/bin/lua
 
+local function create_interval(start_str, len_str)
+	local len = tonumber(len_str)
+	local start = tonumber(start_str)
+	return {
+		min = start,
+		max = start + (len - 1)
+	}
+end
 
-local phase = {
-	read_header = 1,
-	read_map = 2
-}
+local function read_part1_seed_intervals(line)
+	local intervals = {}
+	for seed in string.gmatch(line, '%d+') do
+		intervals[#intervals+1] = create_interval(seed, 1)
+	end
+	return intervals
+end
 
-local function read_header(state, line)
-	local match = string.match(line, '([%s%a-]+):')
-	if match == 'seeds' then
-		for seed in string.gmatch(line, '%d+') do
-			state.seeds[#state.seeds+1] = tonumber(seed)
+local function read_part2_seed_intervals(line)
+	local intervals = {}
+	for start, len in string.gmatch(line, '(%d+) (%d+)') do
+		intervals[#intervals+1] = create_interval(start, len)
+	end
+	return intervals
+end
+
+local function advance_interval(i, min, max, delta)
+	local results = {}
+	if min <= i.min and i.max <= max then
+		results[#results+1] = {
+			eligible = false,
+			min = i.min + delta,
+			max = i.max + delta
+		}
+	elseif i.min <= min and max <= i.max then
+		results[#results+1] = {
+			eligible = true,
+			min = i.min,
+			max = min - 1
+		}
+		results[#results+1] = {
+			eligible = false,
+			min = min + delta,
+			max = max + delta
+		}
+		results[#results+1] = {
+			eligible = true,
+			min = max + 1,
+			max = i.max
+		}
+	elseif min <= i.min and max >= i.min and max <= i.max then
+		results[#results+1] = {
+			eligible = false,
+			min = i.min + delta,
+			max = max + delta
+		}
+		results[#results+1] = {
+			eligible = true,
+			min = max + 1,
+			max = i.max
+		}
+	elseif min >= i.min and i.max >= min and i.max <= max then
+		results[#results+1] = {
+			eligible = true,
+			min = i.min,
+			max = min - 1
+		}
+		results[#results+1] = {
+			eligible = false,
+			min = min + delta,
+			max = i.max + delta
+		}
+	else
+		results[#results+1] = i
+	end
+	return results
+end
+
+local map_pattern = '(%d+) (%d+) (%d+)'
+local function advance_intervals(intervals, line)
+	if string.match(line, ':') then
+		for _, interval in ipairs(intervals) do
+			interval.eligible = true
 		end
-		for start_match, len_match in string.gmatch(line, '(%d+) (%d+)') do
-			local start = tonumber(start_match)
-			local len = tonumber(len_match) - 1
-			for i = start, start + len do
-				print(i)
-				state.seeds2[#state.seeds2+1] = i
+		return intervals
+	end
+
+	local dest, src, len = string.match(line, map_pattern)
+	if not dest or not src or not len then
+		return intervals
+	end
+
+	local new_intervals = {}
+	local min = tonumber(src)
+	local delta = tonumber(dest) - min
+	local max = min + (tonumber(len) - 1)
+
+	for _, interval in ipairs(intervals) do
+		if interval.eligible then
+			local results = advance_interval(interval, min, max, delta)
+			for _, new_interval in ipairs(results) do
+				new_intervals[#new_intervals+1] = new_interval
 			end
-		end
-		return phase.read_header
-	end
-
-	local src, dest = string.match(line, '(%a+)%-to%-(%a+)')
-	if src and dest then
-		state.reading_src = src
-		state.reading_dest = dest
-		--print('reading map: ' .. (src or 'nil') .. ' to ' .. (dest or 'nil'))
-		return phase.read_map
-	end
-end
-
-local function read_map(state, line)
-	local pattern =  '(%d+)%s*(%d+)%s*(%d+)'
-	local dest_match, src_match, len_match = string.match(line, pattern)
-
-	local dest_start = tonumber(dest_match)
-	local src_start = tonumber(src_match)
-	local len = tonumber(len_match)
-	local delta = dest_start - src_start
-
-	local reading_dest = state.reading_dest
-	local predecessor = state.maps[state.reading_src]
-	state.maps[state.reading_src] = function(input)
-		if input >= src_start and input <= src_start + (len - 1) then
-			return reading_dest, input + delta
-		elseif predecessor then
-			return predecessor(input)
 		else
-			return reading_dest, input
+			new_intervals[#new_intervals+1] = interval
 		end
 	end
 
-	return phase.read_map
+	return new_intervals
 end
 
-local phases = {
-	read_header,
-	read_map
-}
+local function solve_intervals(intervals)
+	local min = nil
+	for _, interval in ipairs(intervals) do
+		if not min or min > interval.min then
+			min = interval.min
+		end
+	end
+	return min
+end
 
 local function day5(path)
-	local state = {
-		maps = {},
-		seeds = {},
-		seeds2 = {},
-		reading_src = nil,
-		reading_dest = nil,
-		phase = phase.read_header
-	}
-
-	for line in io.lines(path) do
-		if string.match(line, '^%s*$') then
-			state.phase = phase.read_header
-		else
-			state.phase = phases[state.phase](state, line)
-		end
+	local lines = io.lines(path)
+	local seed_data = lines()
+	local intervals1 = read_part1_seed_intervals(seed_data)
+	local intervals2 = read_part2_seed_intervals(seed_data)
+	for line in lines do
+		intervals1 = advance_intervals(intervals1, line)
+		intervals2 = advance_intervals(intervals2, line)
 	end
-	local part1 = nil
-	for _, seed in ipairs(state.seeds) do
-		local value = seed
-		local type = 'seed'
-		while type ~= 'location' do
-			type, value = state.maps[type](value)
-		end
-		if not part1 or part1 > value then
-			part1 = value
-		end
-	end
-	print('part 1: ', part1)
-	local part2 = nil
-	for _, seed in ipairs(state.seeds2) do
-		local value = seed
-		local type = 'seed'
-		while type ~= 'location' do
-			type, value = state.maps[type](value)
-		end
-		if not part2 or part2 > value then
-			part2 = value
-		end
-	end
-	print('part 2: ', part2)
+	print('part 1: ', solve_intervals(intervals1))
+	print('part 2: ', solve_intervals(intervals2))
 end
 
 day5(arg[1] or 'input')
-
-
