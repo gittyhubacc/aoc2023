31347ffd059abb8da553a5a2b7ba9515d3277abe
based <based@arch>
2023-12-10 14:41:23 -0500

    day 10

--- day10/day10.lua
+++ day10/day10.lua
@@ -1,5 +1,76 @@
 #!/usr/bin/lua
 
+local dir = {
+	north = { 0, -1 },
+	south = { 0, 1 },
+	west = { -1, 0 },
+	east = { 1, 0 }
+}
+
+-- test if the delta we used to arrive at ch
+-- is appropriate for it's shape
+local function appropriate(ch, dx, dy)
+	if ch == '|' then
+		return dx == 0
+	elseif ch == '-' then
+		return dy == 0
+	elseif ch == 'L' then
+		return dx == -1 or dy == 1
+	elseif ch == 'J' then
+		return dx == 1 or dy == 1
+	elseif ch == '7' then
+		return dx == 1 or dy == -1
+	elseif ch == 'F' then
+		return dx == -1 or dy == -1
+	end
+end
+
+-- return the direction ch faces
+-- when it's approached from dx/dy
+local function facing(ch, dx, dy)
+	if ch == '|' then
+		return dy == 1 and dir.south or dir.north
+	elseif ch == '-' then
+		return dx == 1 and dir.east or dir.west
+	elseif ch == 'L' then
+		return dx == -1 and dir.north or dir.east
+	elseif ch == 'J' then
+		return dx == 1 and dir.north or dir.west
+	elseif ch == '7' then
+		return dx == 1 and dir.south or dir.west
+	elseif ch == 'F' then
+		return dx == -1 and dir.south or dir.east
+	end
+end
+
+-- replace the S with the appropriate pipe part
+local function replace_s(grid, x, y)
+	local linked = {}
+	for name, delta in pairs(dir) do
+		local dx, dy = table.unpack(delta)
+		local ch = grid[y + dy] and grid[y + dy][x + dx] or nil
+		linked[name] = appropriate(ch, dx, dy)
+	end
+	if linked.north then
+		if linked.south then
+			return '|'
+		elseif linked.east then
+			return 'L'
+		elseif linked.west then
+			return 'J'
+		end
+	elseif linked.south then
+		if linked.east then
+			return 'F'
+		elseif linked.west then
+			return '7'
+		end
+	else
+		return '-'
+	end
+end
+
+-- read input into a grid
 local function load_grid(path)
 	local y = 1
 	local sx, sy
@@ -17,179 +88,141 @@ local function load_grid(path)
 		end
 		y = y + 1
 	end
+	grid[sy][sx] = replace_s(grid, sx, sy)
 	return grid, sx, sy
-end
-
-local dir = {
-	north = { 0, -1 },
-	south = { 0, 1 },
-	west = { -1, 0 },
-	east = { 1, 0 }
-}
-
-local function facing(ch, dx, dy)
-	if ch == '|' then
-		return dy == 1 and dir.south or dir.north
-	elseif ch == '-' then
-		return dx == 1 and dir.east or dir.west
-	elseif ch == 'L' then
-		return dx == -1 and dir.north or dir.east
-	elseif ch == 'J' then
-		return dx == 1 and dir.north or dir.west
-	elseif ch == '7' then
-		return dx == 1 and dir.south or dir.west
-	elseif ch == 'F' then
-		return dx == -1 and dir.south or dir.east
-	end
-end
-
-local function appropriate(ch, dx, dy)
-	if ch == '|' then
-		return dx == 0
-	elseif ch == '-' then
-		return dy == 0
-	elseif ch == 'L' then
-		return dx == -1 or dy == 1
-	elseif ch == 'J' then
-		return dx == 1 or dy == 1
-	elseif ch == '7' then
-		return dx == 1 or dy == -1
-	elseif ch == 'F' then
-		return dx == -1 or dy == -1
-	else
-		return ch == 'S'
-	end
 end
 
 local function hash(x, y)
 	return x .. ',' .. y
 end
 
-local function reverse_hash(h)
-	return string.match(h, '(%d+),(%d+)')
-end
-
-local function walk_grid(grid, graph, rx, ry, dx, dy, i)
-	local x = rx + dx
-	local y = ry + dy
+local function walk_grid(grid, graph, x, y, dx, dy, i)
 	local ch = grid[y] and grid[y][x] or nil
 	if not ch or not appropriate(ch, dx, dy) then
 		return nil, false, i
 	end
+
 	local looped = false
-	if ch ~= 'S' then
+	local key = hash(x, y)
+	if not graph[key] then
 		local node = { ch = ch }
-		local fx, fy = table.unpack(facing(ch, dx, dy))
-		node.child, looped, i = walk_grid(grid, graph, x, y, fx, fy, i + 1)
-		graph[hash(x,y)] = node
+		node.dir = facing(ch, dx, dy)
+		local fx, fy = table.unpack(node.dir)
+		node.child, looped, i = walk_grid(grid, graph, x + fx, y + fy, fx, fy, i + 1)
+		graph[key] = node
 	else
 		looped = true
 	end
-	return graph[hash(x,y)], looped, i
+	return graph[key], looped, i
 end
 
 local function load_graph(grid, x, y)
 	local root = {}
 	local graph = {}
-	graph[hash(x,y)] = root
-	local printed = false
+	graph.root = root
+	graph[hash(x, y)] = root
 	for name, delta in pairs(dir) do
 		local dx, dy = table.unpack(delta)
-		local node, loop, i = walk_grid(grid, graph, x, y, dx, dy, 1)
+		local node, loop, i = walk_grid(grid, graph, x + dx, y + dy, dx, dy, 1)
 		if loop then
+			graph.deep = i
+			root.dir = delta
 			root[name] = node
-			if not printed then
-				printed = true
-				print('part 1: ', math.floor(i / 2))
-			end
+			root.ch = grid[y][x]
+			break
 		end
 	end
 	return graph
 end
 
-local function add_to_group(grid, graph, group, group_map, x, y)
-	if not grid[y] or not grid[y][x] then
-		return
-	end
-	group[hash(x,y)] = group
-	group_map[hash(x,y)] = group
-	for ny = -1, 1 do
-		for nx = -1, 1 do
-			local idx = hash((x + nx) ,(y + ny))
-			if not graph[idx] and not group_map[idx] and (x ~= 0 and y ~= 0) then
-				add_to_group(grid, graph, group, group_map, x + nx, y + ny)
-			end
+-- returns directions from which you are on the "left" of ch
+-- takes into consideration the direction ch was facing when it was "laid"
+local function left_deltas(ch, d)
+	if ch == '|' then
+		if d == dir.north then
+			return {{1, 0}}
+		else
+			return {{-1, 0}}
+		end
+	elseif ch == '-' then
+		if d == dir.east then
+			return {{0, 1}}
+		else
+			return {{0, -1}}
+		end
+	elseif ch == 'L' then
+		if d == dir.north then
+			return {{1, 0}, {0, -1}}
+		else
+			return {}
+		end
+	elseif ch == 'J' then
+		if d == dir.west then
+			return {{-1, 0}, {0, -1}}
+		else
+			return {}
+		end
+	elseif ch == '7' then
+		if d == dir.south then
+			return {{0, 1}, {-1, 0}}
+		else
+			return {}
+		end
+	elseif ch == 'F' then
+		if d == dir.east then
+			return {{1, 0}, {0, 1}}
+		else
+			return {}
 		end
 	end
 end
 
-local function crossed(ch, dx, dy)
-	if ch == '-' then
-		return dy ~= 0
-	elseif ch == '|' then
-		return dx ~= 0
-	end
-	return false
-end
 
-local function can_escape(grid, graph, x, y)
-	for name, delta in pairs(dir) do
-		local bad_dir = false
+-- is the coordinate passed on the "left" or "right" side of the main loop
+local function left_or_right(grid, graph, x, y)
+	for _, delta in pairs(dir) do
 		local dx, dy = table.unpack(delta)
 		local cx, cy = x + dx, y + dy
 		while grid[cy] and grid[cy][cx] do
-			local ch = grid[cy][cx]
 			local node = graph[hash(cx, cy)]
-			if node and crossed(node.ch, dx, dy) then
-				--print('crossed at: ' .. cx .. ', ' .. cy, node.ch)
-				--print('dx,dy = ', dx, dy)
-				bad_dir = true
-				break
+			if node then
+				for _, dp in ipairs(left_deltas(node.ch, node.dir)) do
+					if dp[1] == dx and dp[2] == dy then
+						return true
+					end
+				end
+				return false
 			end
 			cx = cx + dx
 			cy = cy + dy
 		end
-		if not bad_dir then
-			print('group can escape! ', x, y)
-			return true
+	end
+	-- the case where a cell does not touch the main loop in any of nsew directions
+	-- whether it's on the left or right in this case is dependent
+	-- on whether we're traversing the main loop clockwise or counter clockwise
+	-- which isn't something i figured out how to reliably define
+	return graph.root.dir == dir.north or graph.root.dir == dir.east
+end
+
+local function count_sides(grid, graph)
+	local left = 0
+	local right = 0
+	for y, row in ipairs(grid) do
+		for x, ch in ipairs(row) do
+			if not graph[hash(x, y)] then
+				if left_or_right(grid, graph, x, y) then
+					left = left + 1
+				else
+					right = right + 1
+				end
+			end
 		end
 	end
-	return false
+	return left, right
 end
 
 local grid, sx, sy = load_grid(arg[1] or 'input')
 local graph = load_graph(grid, sx, sy)
-local group_map = {}
-local groups = {}
-for y, row in ipairs(grid) do
-	for x, ch in ipairs(row) do
-		if not graph[hash(x,y)] and not group_map[hash(x,y)] then
-			local group = {}
-			groups[#groups+1] = group
-			add_to_group(grid, graph, group, group_map, x, y)
-		end
-	end
-end
-
-local part2 = 0
-print('found ' .. #groups .. ' groups')
-for i, group in ipairs(groups) do
-	local group_total = 0
-	local group_inside = true
-	for key, _ in pairs(group) do
-		group_total = group_total + 1
-		local x, y = reverse_hash(key)
-		if group_inside and can_escape(grid, graph, x, y) then
-			group_inside = false
-		end
-	end
-	if group_inside then
-		part2 = part2 + group_total
-	end
-end
-print('part 2: ', part2)
-
-
-
-
+print('part 1: ', math.floor(graph.deep / 2))
+print('part 2: ', count_sides(grid, graph))
 
